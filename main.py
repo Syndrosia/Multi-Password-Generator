b042bf7ef34cedb15da173fd209d25489df75fca
RS6 <88463690+Syndrosia@users.noreply.github.com>
2022-09-22 22:32:43 +0100

    Made it 9 lines!

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ amount, length, sym, tmp   = int(input("Enter amount of passwords to be generate
 for o in range(amount):
    for i in range(length):
       x, s, l, n = r.randint(0,2), r.randint(0,34), r.randint(0,25), r.randint(0,9)
-      if x == 0:
-         tmp = tmp + str(sym[x][s])
-      elif x == 1:
-         tmp = tmp + str(sym[x][l])
-      else: tmp = tmp + str(sym[x][n])    
-   print(tmp)
-   tmp = ""
+      if x == 0: tmp = tmp + str(sym[x][s])
+      elif x == 1: tmp = tmp + str(sym[x][l])
+      else: tmp = tmp + str(sym[x][n])
+   print(tmp); tmp = ""
