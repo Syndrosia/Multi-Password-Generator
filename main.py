a500f7dad247d4fa69d665595ed3e7cff7750394
RS6 <88463690+Syndrosia@users.noreply.github.com>
2022-09-22 22:26:15 +0100

    Update main.py
    Made it 12 lines long!

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import random as r
-amount = int(input("Enter amount of passwords to be generated: "))
-length = int(input("Enter the length of the password to be generated: "))
-sym = [["!","@","£","$","%","^","&","*","(",")","-", "_", "+","=", "`", "¬", "¦", "'", "[", "]", ";", ":", "{", "}", "~", "#", ",", "<", ".",">", "/","?","|", " ", ""], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"], ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]]
-print("Generating passwords...")
-tmp = ""
+amount, length, sym, tmp   = int(input("Enter amount of passwords to be generated: ")), int(input("Enter the length of the password to be generated: ")), [["!","@","£","$","%","^","&","*","(",")","-", "_", "+","=", "`", "¬", "¦", "'", "[", "]", ";", ":", "{", "}", "~", "#", ",", "<", ".",">", "/","?","|", " ", ""], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"], ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]], ""
 for o in range(amount):
    for i in range(length):
-      x = r.randint(0,2)
-      s = r.randint(0,34)
-      l = r.randint(0,25)
-      n = r.randint(0,9)
+      x, s, l, n = r.randint(0,2), r.randint(0,34), r.randint(0,25), r.randint(0,9)
       if x == 0:
          tmp = tmp + str(sym[x][s])
       elif x == 1:
          tmp = tmp + str(sym[x][l])
-      elif x == 2:
-         tmp = tmp + str(sym[x][n])
+      else: tmp = tmp + str(sym[x][n])    
    print(tmp)
    tmp = ""
