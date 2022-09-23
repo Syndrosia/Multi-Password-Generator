75da8fc76899714b3ecee44f8a2a02786b667088
RS6 <88463690+Syndrosia@users.noreply.github.com>
2022-09-23 19:25:45 +0100

    Made it only 7 lines long!
    variable compression and packing at its peak

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import random as r
-amount, length, sym, tmp = int(input("Enter amount of passwords to be generated: ")), int(input("Enter the length of the password to be generated: ")), [["!","@","£","$","%","^","&","*","(",")","-", "_", "+","=", "`", "¬", "¦", "'", "[", "]", ";", ":", "{", "}", "~", "#", ",", "<", ".",">", "/","?","|", " ", ""], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"], ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]], ""
+amount, length, sym, tmp = int(input("Enter amount of passwords to be generated: ")), int(input("Enter the length of the password to be generated: ")), ["!","@","£","$","%","^","&","*","(",")","-", "_", "+","=", "`", "¬", "¦", "'", "[", "]", ";", ":", "{", "}", "~", "#", ",", "<", ".",">", "/","?","|", " ", "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ""
 for o in range(amount):
    for i in range(length):
-      x, s, l, n = r.randint(0,2), r.randint(0,34), r.randint(0,25), r.randint(0,9)
-      if x == 0: tmp = tmp + str(sym[x][s])
-      elif x == 1: tmp = tmp + str(sym[x][l])
-      else: tmp = tmp + str(sym[x][n])
+      s = r.randint(0,68)
+      tmp = tmp + sym[s]
    print(tmp); tmp = ""
