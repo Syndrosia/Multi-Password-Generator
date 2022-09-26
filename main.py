3842d0cbf655ea7a600122fdc4505e9908e452d5
RS6 <88463690+Syndrosia@users.noreply.github.com>
2022-09-26 20:40:17 +0100

    Made it only 5 lines long!
    very compressed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random as r
-amount, length, sym, tmp = int(input("Enter amount of passwords to be generated: ")), int(input("Enter the length of the password to be generated: ")), ["!","@","£","$","%","^","&","*","(",")","-", "_", "+","=", "`", "¬", "¦", "'", "[", "]", ";", ":", "{", "}", "~", "#", ",", "<", ".",">", "/","?","|", " ", "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], ""
-for o in range(amount):
-   for i in range(length):
-      s = r.randint(0,68)
-      tmp = tmp + sym[s]
+import random as r; tmp, v, t = "", int(input("Enter amount of passwords to be generated: ")), int(input("Enter the length of the password to be generated: "))
+for o in range(v):
+   for i in range(t):
+      tmp = tmp + str(r.choice(["!","@","£","$","%","^","&","*","(",")","-", "_", "+","=", "`", "¬", "¦", "'", "[", "]", ";", ":", "{", "}", "~", "#", ",", "<", ".",">", "/","?","|", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]))
    print(tmp); tmp = ""
